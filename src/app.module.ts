23f6b5b035a2adac0fea2b7b2bdf87dc53b0e686
Rajesh <rajesh.yadav@techsierra..in>
2024-12-30 11:58:17 +0530

    Mongo DB is connected in the lcoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [MongooseModule.forRoot(process.env.DB_URI), AuthModule],
   controllers: [],
   providers: [],
 })
