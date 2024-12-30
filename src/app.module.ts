66ba0ce24d7b78b0317bd4e2f63f14037aa8cbcf
Rajesh <rajesh.yadav@techsierra..in>
2024-12-30 12:12:51 +0530

    set the env file for global level

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DB_URI), AuthModule],
+  imports: [
+     ConfigModule.forRoot({
+      envFilePath : '.env',
+      isGlobal : true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    AuthModule],
   controllers: [],
   providers: [],
 })
