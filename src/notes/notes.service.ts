18de7b73b8eabb666b061e41608673f55a36909d
Rajesh <rajesh.yadav@techsierra..in>
2024-12-31 18:37:20 +0530

    handled the error

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -12,7 +12,11 @@ export class NotesService {
     ) {}
 
     async createNote(note: Notes, user: User) {
-        const newNote = await this.noteModel.create({ ...note, user: user._id });
-        return newNote;
+        try {
+            const newNote = await this.noteModel.create({ ...note, user: user._id });
+            return newNote;
+        } catch (error) {
+            return error;
+        }
     }
 }
